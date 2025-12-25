e1ed0e93994ab55772855b8b08b4834ffb01d204
ayxan777-e <eliyevayxan2006@gmail.com>
2025-12-26 01:04:24 +0400

    Add project files.

---
+++ Hospital/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Data.Contexts;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(
+            "Server=DESKTOP-H42M0B3\\SQLEXPRESS01;Database=HospitalDb;Trusted_Connection=True;TrustServerCertificate=True");
+    }
+}
---
+++ Hospital/Program.cs
@@ -0,0 +1,6 @@
+﻿using Hospital.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+AppDbContext _context = new();
