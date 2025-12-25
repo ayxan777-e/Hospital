a7813c8df764ab5694c639ffce299a0c0eec5f9b
ayxan777-e <eliyevayxan2006@gmail.com>
2025-12-26 01:07:05 +0400

    created Patient and Doctor classes

---
+++ Hospital/Data/Entities/Doctors.cs
@@ -0,0 +1,8 @@
+﻿namespace Hospital.Data.Entities;
+
+public class Doctors
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public string Specialty { get; set; } = null!;
+}
---
+++ Hospital/Data/Entities/Patients.cs
@@ -0,0 +1,8 @@
+﻿namespace Hospital.Data.Entities;
+
+public class Patients
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+}
